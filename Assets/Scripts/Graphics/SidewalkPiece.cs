9f27986bc65fa0b246a03b696fff78820e3860aa
Stef Pinto <stefanopinto1@gmail.com>
2026-01-27 23:19:28 -0800

    Sidewalk shader prototyping

--- Assets/Scripts/Graphics/SidewalkPiece.cs
+++ Assets/Scripts/Graphics/SidewalkPiece.cs
@@ -5,21 +5,20 @@ using UnityEditor.Rendering;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class RoadPiece : MonoBehaviour
+public class SidewalkPieces : MonoBehaviour
 {
     [SerializeField] private bool _isUsingManualOffset = false;
-    [Range(1, 4 )]
+    [Range(1, 2 )]
     [SerializeField] private float _offsetX = 0.0f;
-    [Range(1, 4)]
+    [Range(1, 2)]
     [SerializeField] private float _offsetY = 0.0f;
-    [Range(1, 22)]
-    [SerializeField] private float _textureArrayIndex = 0.0f;
+    private float _flippedUV = 0.0f;
 
     private MaterialPropertyBlock _materialPropertyBlock = null;
 
     private void Start()
     {
-        
+        _flippedUV = Random.Range(0.0f, 1.0f);  
     }
 
 
@@ -33,6 +32,7 @@ public class RoadPiece : MonoBehaviour
         _materialPropertyBlock.SetFloat("_Manual_Offset", _isUsingManualOffset ? 1.0f : 0.0f);
         _materialPropertyBlock.SetFloat("_xOffset", _offsetX);
         _materialPropertyBlock.SetFloat("_yOffset", _offsetY);
+        _materialPropertyBlock.SetFloat("_flippedUV", _flippedUV);
 
         renderer.SetPropertyBlock( _materialPropertyBlock );
     }
