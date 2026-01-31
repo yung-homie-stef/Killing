5c8a0dd24bf9792714b42aeeccdf9193ba2a066c
Stef Pinto <stefanopinto1@gmail.com>
2026-01-30 22:18:30 -0800

    Sidewalk outline for starter alleyway

--- Assets/Scripts/Graphics/RoadPiece.cs
+++ Assets/Scripts/Graphics/RoadPiece.cs
@@ -8,12 +8,13 @@ using UnityEngine;
 public class RoadPiece : MonoBehaviour
 {
     [SerializeField] private bool _isUsingManualOffset = false;
+    [SerializeField] private bool _isUsingTextureArray = false;
     [Range(1, 4 )]
     [SerializeField] private float _offsetX = 0.0f;
     [Range(1, 4)]
     [SerializeField] private float _offsetY = 0.0f;
     [Range(1, 22)]
-    [SerializeField] private float _textureArrayIndex = 0.0f;
+    [SerializeField] private int _textureArrayIndex = 0;
 
     private MaterialPropertyBlock _materialPropertyBlock = null;
 
@@ -31,8 +32,10 @@ public class RoadPiece : MonoBehaviour
         Renderer renderer = GetComponent<Renderer>();
 
         _materialPropertyBlock.SetFloat("_Manual_Offset", _isUsingManualOffset ? 1.0f : 0.0f);
+        _materialPropertyBlock.SetFloat("_Texture_Array", _isUsingTextureArray ? 1.0f : 0.0f);
         _materialPropertyBlock.SetFloat("_xOffset", _offsetX);
         _materialPropertyBlock.SetFloat("_yOffset", _offsetY);
+        _materialPropertyBlock.SetFloat("_textureIndex", _textureArrayIndex);
 
         renderer.SetPropertyBlock( _materialPropertyBlock );
     }
