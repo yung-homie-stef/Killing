e64a160d9c8dc0d8d157ba94618d6415bf466bf6
Stef Pinto <stefanopinto1@gmail.com>
2026-03-05 21:33:42 -0800

    Two tabs for inventory UI
    Clicking on (temp) buttons now swaps between layout groups for key items and standard inventory items. Item descriptions and names update accordingly when either tab is swapped to.

--- Assets/Scripts/UI/Inventory/InventoryUI.cs
+++ Assets/Scripts/UI/Inventory/InventoryUI.cs
@@ -12,15 +12,19 @@ public class InventoryUI : MonoBehaviour
     [SerializeField] private GameObject _content;
     [SerializeField] private GridLayoutGroup _inventoryLayoutGroup;
     [SerializeField] private GridLayoutGroup _keyItemInventoryLayoutGroup;
-
     [SerializeField] private GameObject _inventoryItemButtonPrefab;
+    [SerializeField] private GameObject _keyItemHierarchy;
+    [SerializeField] private GameObject _standardItemHierarchy;
+
+    [Header("Text")]
     [SerializeField] private TextMeshProUGUI _itemDescriptionText;
     [SerializeField] private TextMeshProUGUI _itemNameText;
     //[SerializeField] private UseDiscardPrompt _useDiscardPrompt;
     [HideInInspector] public Interactable targetInteractable = null;
 
-    [Header("Buttons")]
+    [Header("Button Prefab Lists")]
     [SerializeField] private List<InventoryItemButton> _inventoryItemButtonList = new List<InventoryItemButton>();
+    [SerializeField] private List<InventoryItemButton> _keyItemButtonList = new List<InventoryItemButton>();
 
     private void OnEnable()
     {
@@ -51,24 +55,50 @@ public class InventoryUI : MonoBehaviour
         Cursor.lockState = mode;
         Cursor.visible = flag;
         _content.SetActive(flag);
+
+        if (flag)
+            CheckIfThereAreAnyItemsToFillDescription(_inventoryItemButtonList);
     }
 
+    public void SwitchItemTab(bool flag)
+    {
+        if (flag) // switching to key items
+        {
+            _keyItemHierarchy.SetActive(true);
+            _standardItemHierarchy.SetActive(false);
+            CheckIfThereAreAnyItemsToFillDescription(_keyItemButtonList);
+        }
+        else
+        {
+            _keyItemHierarchy.SetActive(false);
+            _standardItemHierarchy.SetActive(true);
+            CheckIfThereAreAnyItemsToFillDescription(_inventoryItemButtonList);
+        }
+    }
 
     public void AddItemToInventoryUI(ItemObject itemObj, InventorySlot slot)
     {
         GridLayoutGroup glg;
+        List<InventoryItemButton> liib;
 
-       if (itemObj.isKeyItem)
-           glg = _keyItemInventoryLayoutGroup;
-       else
-           glg = _inventoryLayoutGroup;
+
+        if (itemObj.isKeyItem)
+        {
+            glg = _keyItemInventoryLayoutGroup;
+            liib = _keyItemButtonList;
+        }
+        else
+        {
+            glg = _inventoryLayoutGroup;
+            liib = _inventoryItemButtonList;
+        }
 
 
         InventoryItemButton _inventoryItemButton = Instantiate(_inventoryItemButtonPrefab, glg.transform).GetComponent<InventoryItemButton>();
         _inventoryItemButton.Initialize(itemObj, slot);
         _inventoryItemButton.transform.SetAsFirstSibling();
         _inventoryItemButton.name = itemObj.name;
-        _inventoryItemButtonList.Add(_inventoryItemButton);
+        liib.Add(_inventoryItemButton);
     }
 
     public void UpdateInventoryItemDisplay(ItemObject itemObj)
@@ -80,11 +110,20 @@ public class InventoryUI : MonoBehaviour
     public void ResetInventoryItemDisplay()
     {
         _itemDescriptionText.text = string.Empty;
+        _itemNameText.text = string.Empty;
     }
 
     public void RemoveItemFromInventoryUI(InventoryItemButton itemButton)
     {
         _inventoryItemButtonList.Remove(itemButton);
         Destroy(itemButton.gameObject);
+    }
+
+    private void CheckIfThereAreAnyItemsToFillDescription(List<InventoryItemButton> list)
+    {
+        if (list.Count > 0)
+            UpdateInventoryItemDisplay(list[^1]._itemObject);
+        else
+            ResetInventoryItemDisplay();
     }
 }
