96cb11768ff189f4d54be732c3b3b273eaf439de
Stef Pinto <stefanopinto1@gmail.com>
2026-05-06 08:48:12 -0700

    Mr. Tsui conversation blocking

--- Assets/Scripts/Interactions/Characters/CharacterInteractable.cs
+++ Assets/Scripts/Interactions/Characters/CharacterInteractable.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using PixelCrushers.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,11 +7,28 @@ using UnityEngine;
 public class CharacterInteractable : Interactable
 {
     [SerializeField] private DialogueSystemTrigger _DS_Trigger;
+    [SerializeField] private CinemachineVirtualCamera _characterVirtualCamera;
+
+    public override void Awake()
+    {
+        DialogueManager.instance.conversationEnded += OnConversationEnded;
+    }
+
+    private void OnDisable()
+    {
+        DialogueManager.instance.conversationEnded += OnConversationEnded;
+    }
 
     public override void Interact()
     {
         base.Interact();
         _DS_Trigger.OnUse();
+        _characterVirtualCamera.Priority = 1;
         GameEventsManager.instance.playerEvents.DisablePlayerMovement();
+    }
+
+    private void OnConversationEnded(Transform t)
+    {
+        _characterVirtualCamera.Priority = 0;
     }
 }
